43ec5fccbf8e42e9650ad0faf9ed619a679e49a7
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-01 12:52:43 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -4,18 +4,11 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 const changeColorBtn = document.querySelector(".change-color");
 const colorEl = document.querySelector(".color");
 const bodyEl = document.querySelector("body");
 
-changeColorBtn.addEventListener("click", () => {
-  colorEl.textContent = getRandomHexColor();
-  bodyEl.style.backgroundColor = colorEl.textContent;
-});
-
+changeColorBtn.addEventListener("click", function () {
+    colorEl.textContent = getRandomHexColor();
+    bodyEl.style.backgroundColor = colorEl.textContent;
+  });
