28f24b2ebdc96905a657f89da6b136ed8ff4633b
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-02 15:50:44 +0200

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,47 @@
-const totalCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${totalCategories.length}`);
+const totalCategories = document.getElementById("categories");
+console.log(`Number of categories: ${totalCategories.children.length}`);
 
-const categoriesArray = [...totalCategories]
+// const titleEl = document.querySelectorAll('h2');
+// titleEl.forEach((listName) => {
+//   console.lof(listName);
+// });
 
-  .map(
-    (categories) => `Category: ${categories.children[0].textContent}
-Elements ${categories.children[1].children.length}`
-  )
+const elems = document.querySelectorAll("h2,.ul");
+elems.forEach((item) => {
+  console.log(`Category: ${item.firstChild.textContent}`);
+  console.log(
+    `Elements: ${item.children[1]}`
+  );
+});
 
-  .join("\n");
-console.log(categoriesArray);
+// const listEl = document.querySelectorAll("ul");
+// listEl.forEach((numbers) => {
+//   console.log(`Elements: ${numbers.children.length}`);
+// });
+
+
+// console.log(item.children.length);
+  
+
+
+// const elemsCount = document.querySelectorAll('li');
+// elemsCount.forEach((count) => {
+//   console.log(count.childNodes.lenght);
+// });
+// const categoriesArray = [...totalCategories]
+
+//   .map(
+//     (categories) => `Category: ${categories.children[0].textContent}
+// Elements ${categories.children[1].children.length}`
+//   )
+
+//   .join("\n");
+// console.log(categoriesArray);
+
+// const totalCategories = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${totalCategories.length}`);
+
+// const categoriesEl = document.getElementsByTagName('ul');
+// const forEach (totalCategories => {
+//   console.log(`Elements: ${categoriesEl.length}`);
+// });
