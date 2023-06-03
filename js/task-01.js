d3161d8ab1ea06afea81054d79500e825096a5db
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-03 21:29:02 +0200

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,46 +1,7 @@
-const totalCategories = document.getElementById("categories");
-console.log(`Number of categories: ${totalCategories.children.length}`);
-
-// const titleEl = document.querySelectorAll('h2');
-// titleEl.forEach((listName) => {
-//   console.lof(listName);
-// });
-
-const elems = document.querySelectorAll("h2");
-elems.forEach((item) => {
-  console.log(`Category: ${item.firstChild.textContent}`);
+const items = document.querySelectorAll(".item");
+console.log(`Number of categories: ${items.length}`);
+console.log(items);
+items.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
-
-
-
-// listEl.forEach((value) => {
-//   console.log(value.children.length);
-// });
-// const listEl = document.querySelectorAll("ul");
-// listEl.forEach((numbers) => {
-//   console.log(`Elements: ${numbers.children.length}`);
-// });
-
-// console.log(item.children.length);
-
-// const elemsCount = document.querySelectorAll('li');
-// elemsCount.forEach((count) => {
-//   console.log(count.childNodes.lenght);
-// });
-// const categoriesArray = [...totalCategories]
-
-//   .map(
-//     (categories) => `Category: ${categories.children[0].textContent}
-// Elements ${categories.children[1].children.length}`
-//   )
-
-//   .join("\n");
-// console.log(categoriesArray);
-
-// const totalCategories = document.querySelectorAll(".item");
-// console.log(`Number of categories: ${totalCategories.length}`);
-
-// const categoriesEl = document.getElementsByTagName('ul');
-// const forEach (totalCategories => {
-//   console.log(`Elements: ${categoriesEl.length}`);
-// });
