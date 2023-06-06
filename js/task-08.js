af90f8ed3df8775840353f2a3257728d16b56b37
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-06 14:12:46 +0200

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,38 @@
+// const form = document.querySelector(".login-form");
+// const useremail = document.querySelector('[type="email"]');
+// const password = document.querySelector('[type="password"]');
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+
+//   if (email.value === "" || password.value === "") {
+//     return alert(`Please fill in all the fields!`);
+//   }
+//   console.log(`Email: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
+
+
+
+
+
+
 const form = document.querySelector(".login-form");
 const useremail = document.querySelector('[type="email"]');
 const password = document.querySelector('[type="password"]');
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
-
-  if (email.value === "" || password.value === "") {
+  const elements = {
+    email: useremail.value,
+    pass: password.value,
+  };
+  if (useremail.value === "" || password.value === "") {
     return alert(`Please fill in all the fields!`);
   }
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-}
+  console.log(elements);
+  form.reset();
+});
