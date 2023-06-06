fc762c0274e9b49fd39d9d61e7257ac1c9089704
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-06 13:44:57 +0200

    update

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,3 @@
-
 const form = document.querySelector(".login-form");
 const useremail = document.querySelector('[type="email"]');
 const password = document.querySelector('[type="password"]');
@@ -7,14 +6,12 @@ form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const {
-    elements: {email, password },
+    elements: { email, password },
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
     return alert(`Please fill in all the fields!`);
-  } 
-
+  }
   console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
-
